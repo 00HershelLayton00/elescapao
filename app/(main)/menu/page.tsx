c3e7a785542b7d391d2a0c30413cc5d9a2c0f381
AndrysDM <andrysmoreno4@gmail.com>
2026-04-04 04:14:48 -0400

    te puse una foto

--- app/(main)/menu/page.tsx
+++ app/(main)/menu/page.tsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image"
 export default function MenuPage() {
 
   const categorias = ["Todos", "Entrada", "Principal", "Postre", "Bebida", "VIP"]
@@ -6,6 +6,13 @@ export default function MenuPage() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-8">📋 Nuestro Menú</h1>
+      <Image
+        src="/images/prueba.png"
+        alt="Descripción de la foto"
+        width={500}
+        height={300}
+        className="rounded-lg"
+      />
     </div>
   )
 }
