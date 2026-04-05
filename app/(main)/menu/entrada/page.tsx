c440276b582799166ce0ea64cb02b06bb94c297c
00HershelLayton00 <hershellayton54797723@gmail.com>
2026-04-05 02:24:04 -0400

    que sueno

--- app/(main)/menu/entrada/page.tsx
+++ app/(main)/menu/entrada/page.tsx
@@ -1,7 +1,26 @@
 import ProductCard from "../ProductCard";
 export default function MenuPage() {
-  const productos = [
-    { nombre: "Ensalada César", desc: "Pollo, lechuga, parmesano", precio: "$12.99",},
+  const productos_entrantes = [
+    {nombre:"Croquetas de pescado",desc:"",precio: "$3000",},
+    {nombre:"Croquetas de Camarón",desc:"",precio: "$3000",},
+    {nombre:"Croqueta de Ahumada",desc:"",precio: "$3000",},
+    {nombre:"Croqueta de Queso",desc:"",precio: "$3000",},
+    {nombre:"Brochetas de Cerdo",desc:"",precio: "$3800",},
+    {nombre:"Brochetas de Pollo",desc:"",precio: "$3800",},
+    {nombre:"Brochetas de vegetales",desc:"",precio: "$3800",},
+    {nombre:"Albóndigas de Cerdo",desc:"100% Carne",precio: "$3000",},
+    {nombre:"Tira cervecera",desc:"Chorizo de cerdo hecho en casa",precio: "$3000",},
+    {nombre:"Ají Relleno",desc:"Pimiento relleno con carne de cerdo y gratinado con queso",precio: "$3000",},
+    {nombre:"Picada Escapa",desc:"Bolita de queso, brocheta, masa frita, chorizo, Croquetas, pizza napolitana",precio: "",}
+  ]
+  const productos_aperitivos=[    
+    {nombre:"Sopa del día de Res",desc:"",precio: "900",},
+    {nombre:"Sopa del día de Pollo",desc:"",precio: "900",},
+    {nombre:"Sopa del día de vegetales",desc:"",precio: "1000",},
+    {nombre:"Aguado de Pollo",desc:"Sopa de arroz",precio: "1000",},
+    {nombre:"Caldo Gallego",desc:"Garvanzo y Chorizo",precio: "",},
+    {nombre:"Potaje de frijol negro/rojo",desc:"",precio: "1000",},
+    {nombre:"Ajiaco",desc:"",precio: "1000",},
   ]
 
   return (
@@ -10,7 +29,16 @@ export default function MenuPage() {
 
       {/* Productos */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {productos.map((p, i) => (
+        {productos_entrantes.map((p, i) => (
+          <ProductCard name={p.nombre} description={p.desc} price={p.precio} />
+        ))}
+      </div>
+
+      <h1 className="text-4xl font-bold text-center mb-8">Aperitivos</h1>
+
+      {/* Productos */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {productos_aperitivos.map((p, i) => (
           <ProductCard name={p.nombre} description={p.desc} price={p.precio} />
         ))}
       </div>
--- app/(main)/menu/principal/page.tsx
+++ app/(main)/menu/principal/page.tsx
@@ -1,17 +1,45 @@
 import ProductCard from "../ProductCard";
 export default function MenuPage() {
-  const productos = [
-    { nombre: "Risotto de Hongos", desc: "Arroz arbóreo, hongos", precio: "$18.99" },
-    { nombre: "Pizza Margarita", desc: "Salsa de tomate, mozzarella, albahaca", precio: "$14.99" },
+  const productos_puerco = [
+    {nombre:"Bistec Encebollado",desc:"",precio:"$3800"},
+    {nombre:"Bistec Natural",desc:"",precio:"$3000"},
+    {nombre:"Oreja de Elefante",desc:"1lb de carne de cerdo empanizado y gratinado con queso",precio:"$4800"},
+    {nombre:"Cerdo campesino",desc:"1lb de carne de cerdo empanizado y relleno con pollo, vegetales y gratinado con queso",precio:"$4900"},
+    {nombre:"Costilla Escapá",desc:"4lb de costilla asada y terminada al carbón/casabe/guarnición de presentación",precio:"$9000",},
+    {nombre:"Lonjas de cerdo criolla",desc:"",precio:"$3800"},
+    {nombre:"Lonjas de cerdo ahumado",desc:"",precio:"$3900"},
+    {nombre:"Fajitas de Cerdo Natural con Vegetales",desc:"",precio:"$3000"},
+    {nombre:"Fajitas de Cerdo Natural",desc:"",precio:"$3000"},
+    {nombre:"Pernil de cerdo asado",desc:"Preguntar al mesero",precio:""},
+    {nombre:"Fricasé de cerdo",desc:"",precio:"$3800"},
+    {nombre:"Cerdo asado entero",desc:"Bajo pedido",precio:"$"},
+    {nombre:"Chuleta Criolla/2lb al carbón",desc:"Cebolla/gratinada con queso/vegetales salteados",precio:"$4000",},
+    {nombre:"Masas de Cerdo Frita",desc:"Doradas/secas",precio:"$3000"},
+    {nombre:"Escapá",desc:"Con cebolla",precio:"$3600",},
+    {nombre:"Braba",desc:"Salsa roja/vegetales/picante",precio:"$3800",},
+  ]
+
+  const Productos_cordero=[
+    {nombre:"Cordero al Corte en su salsa",desc:"Sellado al carbón y cocinado en cerveza más agregados secretos",precio:"$4800",},
+    {nombre:"Pierna de Cordero al carbón",desc:"Salsa roja/vegetales/picante",precio:"$8000",},
+    {nombre:"Costilla de Obejo al carbón",desc:"Salsa roja/vegetales/picante",precio:"",},
   ]
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold text-center mb-8">Principal</h1>
-
-      {/* Productos */}
+      
+      <h1 className="text-4xl font-bold text-center mb-8">Plato Fuerte de Cerdo</h1>
+      {/* productos_puerco*/}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {productos.map((p, i) => (
+        {productos_puerco.map((p, i) => (
+          <ProductCard name={p.nombre} description={p.desc} price={p.precio} />
+        ))}
+      </div>
+
+      <h1 className="text-4xl font-bold text-center mb-8">Plato Fuerte de Cordero</h1>
+      {/* productos_puerco*/}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {Productos_cordero.map((p, i) => (
           <ProductCard name={p.nombre} description={p.desc} price={p.precio} />
         ))}
       </div>
