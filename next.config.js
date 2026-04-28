7e6358e9deef4ce12a4e7f5887b9e4a56ae9080b
AndrysDM <andrysmoreno4@gmail.com>
2026-04-28 15:03:41 -0400

    standalone

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // Esto habilita la exportación estática
-  images: {
-    unoptimized: true, // Necesario para imágenes estáticas
-  },
-  // Opcional: Si tienes rutas dinámicas
-  trailingSlash: true, // Mejora compatibilidad con servidores estáticos
-}
+//standalone
+/** @type{import('next').NextConfig} */
 
-module.exports = nextConfig
+const nextConfig = {
+  output: 'standalone',
+  // Si usas imágenes (next/image), Tailwind se encarga del CSS, 
+  // pero las imágenes necesitan este ajuste si no hay servidor de optimización:
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
+
+
+
+
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',  // Esto habilita la exportación estática
+//   images: {
+//     unoptimized: true, // Necesario para imágenes estáticas
+//   },
+//   // Opcional: Si tienes rutas dinámicas
+//   trailingSlash: true, // Mejora compatibilidad con servidores estáticos
+// }
+
+// module.exports = nextConfig
